f6e75977d082234ed0539defbd0fdc772ab1d5ea
MayneFarias <168032487+MayneFarias@users.noreply.github.com>
2024-05-06 18:34:51 -0300

    projeto
    Tentando um projeto novo

---
+++ main.py
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compartible" content=" chrome=1
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Listas</title>
+</head>
+<body>
+<H1>Listas ordenadas</H1>
+<ol>
+<li>Acordar as hrs </li>
+<li>Escovar os dentes</li>
+<li>Tomar banho</li>
+<li>Ir para o curso</li>
+</ol>
+<h1>Não Ordenadas</h1>
+<ul>
+<li>Comprar Azeitona </li>
+<li>Comprar banana</li>
+<li>Comprar manga</li>
+<li>comprar cenoura</li>
+</ul>
+</body>
+</html>
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
